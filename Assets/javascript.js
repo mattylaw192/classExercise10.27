c2ec142468c43b9228b56a17a2130dda849ab957
Mattylaw192 <mattylaw192@gmail.com>
2018-10-27 10:51:58 -0400

    Added form to html

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,1 +1,8 @@
-javascript
+// Code for handling the push
+database.ref().push({
+	name: name,
+	email: email,
+	age: age,
+	comment: comment,
+	dateAdded: firebase.database.ServerValue.TIMESTAMP
+});
