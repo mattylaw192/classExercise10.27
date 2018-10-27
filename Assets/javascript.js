b137e8e9ebbecc0ba0f99e3c25ff8c3c099db7d0
Mattylaw192 <mattylaw192@gmail.com>
2018-10-27 10:56:12 -0400

    Added firebase code to html & js

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,4 +1,18 @@
 // Code for handling the push
+/ Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyAbfpDmpf6HG7Z0g29QsjRDbTmgBPFIR6s",
+   authDomain: "class27-d2ae6.firebaseapp.com",
+   databaseURL: "https://class27-d2ae6.firebaseio.com",
+   projectId: "class27-d2ae6",
+   storageBucket: "class27-d2ae6.appspot.com",
+   messagingSenderId: "206793102901"
+ };
+ firebase.initializeApp(config);
+
+
+
+
 database.ref().push({
 	name: name,
 	email: email,
